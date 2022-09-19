108f7308cdb9acf9adb095813054b770c3d1c419
Tasdik Rahman <tasdik.rahman@deliveryhero.com>
2022-09-19 17:37:25 +0200

    wip: Add GetContainerImageForK8sObject function which returns back the whole container image of the deployment/daemonset object passed
    TODO: Add spec for it, and decouple the init of the k8s client set out side of it as part of adding the unit tests for it

--- go.mod
+++ go.mod
@@ -12,7 +12,11 @@ require (
 	github.com/stretchr/testify v1.7.0
 )
 
-require github.com/spf13/viper v1.10.1
+require (
+	github.com/spf13/viper v1.10.1
+	k8s.io/apimachinery v0.21.0
+	k8s.io/client-go v0.21.0
+)
 
 require (
 	github.com/aws/aws-sdk-go-v2/credentials v1.8.0 // indirect
@@ -26,24 +30,44 @@ require (
 	github.com/aws/smithy-go v1.10.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/googleapis/gnostic v0.4.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/kr/pretty v0.2.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.21.0 // indirect
+	k8s.io/klog/v2 v2.70.1 // indirect
+	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
--- internal/api/k8s/cluster.go
+++ internal/api/k8s/cluster.go
@@ -1,10 +1,19 @@
 package k8s
 
 import (
+	"context"
 	"errors"
+	"flag"
 	"fmt"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 	"log"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -78,5 +87,72 @@ func SetK8sContext(clusterName string) {
 	err := cmd.Run()
 	if err != nil {
 		log.Fatalln("Error setting kube context to the cluster selected")
+	}
+}
+
+// buildConfigFromFlags returns the config using which the client will be initialized with the k8s context we want to use
+func buildConfigFromFlags(context, kubeconfigPath string) (*rest.Config, error) {
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfigPath},
+		&clientcmd.ConfigOverrides{
+			CurrentContext: context,
+		}).ClientConfig()
+}
+
+// GetContainerImageForK8sObject is used to return  the container image from for the object
+// Supports deployment and Daemonsets as of now for apps/v1 api
+//
+// Usage:
+// image, err := k8s.GetContainerImageForK8sObject("cluster-name", "aws-node", "daemonset", "kube-system")
+func GetContainerImageForK8sObject(kubeContext, k8sObjectName, k8sObject, namespace string) (string, error) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := buildConfigFromFlags(kubeContext, *kubeconfig)
+	if err != nil {
+		return "", errors.New("error building the config for building the client-set for client-go")
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return "", errors.New("error building the client-set for client-go")
+	}
+
+	if k8sObject == "deployment" {
+		// NOTE: Not targeting other api versions for the objects as of now.
+		deployment, err := clientset.AppsV1().Deployments(namespace).Get(context.TODO(), k8sObjectName, metav1.GetOptions{})
+		if k8sErrors.IsNotFound(err) {
+			return "", errors.New(fmt.Sprintf("Deployment %s in namespace %s not found\n", k8sObjectName, namespace))
+		} else if statusError, isStatus := err.(*k8sErrors.StatusError); isStatus {
+			return "", errors.New(fmt.Sprintf("Error getting deployment %s in namespace %s: %v\n",
+				k8sObjectName, namespace, statusError.ErrStatus.Message))
+		} else if err != nil {
+			return "", errors.New("there was an error while retrieving the container image")
+		}
+
+		// NOTE: This assumes there is only one container in the k8s object, which is true for the components for us at moment
+		return deployment.Spec.Template.Spec.Containers[0].Image, nil
+	} else if k8sObject == "daemonset" {
+		// NOTE: Not targeting other api versions for the objects as of now.
+		daemonset, err := clientset.AppsV1().DaemonSets(namespace).Get(context.TODO(), k8sObjectName, metav1.GetOptions{})
+		if k8sErrors.IsNotFound(err) {
+			return "", errors.New(fmt.Sprintf("Daemonset %s in namespace %s not found\n", k8sObjectName, namespace))
+		} else if statusError, isStatus := err.(*k8sErrors.StatusError); isStatus {
+			return "", errors.New(fmt.Sprintf("Error getting deployment %s in namespace %s: %v\n",
+				k8sObjectName, namespace, statusError.ErrStatus.Message))
+		} else if err != nil {
+			return "", errors.New("there was an error while retrieving the container image")
+		}
+
+		// NOTE: This assumes there is only one container in the k8s object, which is true for the components for us at moment
+		return daemonset.Spec.Template.Spec.Containers[0].Image, nil
+	} else {
+		return "", errors.New("please choose between Daemonset or Deployment k8sobject as they are currently supported")
 	}
 }
